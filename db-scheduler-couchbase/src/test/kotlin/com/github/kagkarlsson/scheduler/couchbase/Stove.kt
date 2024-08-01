a145fbc46c52682ce20f2cb553fa90def315a42b
Oguzhan Soykan <oguzhansoykan@gmail.com>
2024-08-01 16:32:17 +0200

    available processors

--- db-scheduler-couchbase/src/test/kotlin/com/github/kagkarlsson/scheduler/couchbase/Stove.kt
+++ db-scheduler-couchbase/src/test/kotlin/com/github/kagkarlsson/scheduler/couchbase/Stove.kt
@@ -24,8 +24,8 @@ private const val DEFAULT_BUCKET = "db-scheduler"
 
 @ExperimentalKotest
 class Stove : AbstractProjectConfig() {
-  override val concurrentSpecs: Int = 10
-  override val concurrentTests: Int = 10
+  override val concurrentSpecs: Int = Runtime.getRuntime().availableProcessors()
+  override val concurrentTests: Int = Runtime.getRuntime().availableProcessors()
 
   override suspend fun beforeProject() = TestSystem {
     enableReuseForTestContainers()
