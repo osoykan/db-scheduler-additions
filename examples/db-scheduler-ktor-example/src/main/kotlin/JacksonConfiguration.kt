f090ecbc251ba7f2a880faf6b1e32e1c3ca7315a
Oguzhan Soykan <oguzhansoykan@gmail.com>
2024-08-19 19:59:48 +0200

    write dates as timestamps:false

--- examples/db-scheduler-ktor-example/src/main/kotlin/JacksonConfiguration.kt
+++ examples/db-scheduler-ktor-example/src/main/kotlin/JacksonConfiguration.kt
@@ -1,13 +1,15 @@
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 object JacksonConfiguration {
   val default: ObjectMapper = JsonMapper.builder()
-    .findAndAddModules()
+    .apply {
+      configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+    }
     .build()
     .registerKotlinModule()
-    .findAndRegisterModules()
     .registerModule(JavaTimeModule())
+    .findAndRegisterModules()
 }
