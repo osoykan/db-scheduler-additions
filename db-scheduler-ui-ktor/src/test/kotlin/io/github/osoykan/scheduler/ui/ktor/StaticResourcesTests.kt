85b1d395a99300891221774d1c3b16fb2558c6a7
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-02-16 12:22:34 +0100

    fix unit test

--- db-scheduler-ui-ktor/src/test/kotlin/io/github/osoykan/scheduler/ui/ktor/StaticResourcesTests.kt
+++ db-scheduler-ui-ktor/src/test/kotlin/io/github/osoykan/scheduler/ui/ktor/StaticResourcesTests.kt
@@ -7,7 +7,6 @@ class StaticResourcesTests :
   FunSpec({
     test("should get resource as stream") {
       val html = getResourceAsText("static/db-scheduler/index.html")
-      html shouldContain "/db-scheduler/assets/index-ed4b70bb.js"
-      html shouldContain "/db-scheduler/assets/index-c49f4573.css"
+      html shouldContain "/db-scheduler/assets/index-"
     }
   })
