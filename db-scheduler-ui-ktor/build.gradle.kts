cccca1fcf7b412e5908bf91a98c5750d3152faeb
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-07-07 12:34:58 +0200

    removed cached vendor
    Squashed 'vendor/db-scheduler-ui/' content from commit fb53a0e
    
    git-subtree-dir: vendor/db-scheduler-ui
    git-subtree-split: fb53a0e9e5111d9fb9128759ae2e12cae30f57e9
    
    integrate db-scheduler-ui

--- db-scheduler-ui-ktor/build.gradle.kts
+++ db-scheduler-ui-ktor/build.gradle.kts
@@ -1,5 +1,24 @@
 plugins {
   kotlin("jvm") version libs.versions.kotlin
+  alias(libs.plugins.node)
+}
+
+val uiPath = project.layout.projectDirectory.dir("src/main/resources/static/scheduler-ui")
+val distPath = uiPath.dir("dist")
+
+node {
+  version.set("24.3.0")
+  download.set(true)
+  npmInstallCommand.set("install")
+  nodeProjectDir.set(uiPath)
+}
+
+tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmBuild") {
+  description = "Run npm build command"
+  args.set(listOf("run", "build"))
+  dependsOn("nodeSetup", "npmSetup", "npmInstall")
+  inputs.dir(uiPath)
+  outputs.dir(distPath)
 }
 
 dependencies {
