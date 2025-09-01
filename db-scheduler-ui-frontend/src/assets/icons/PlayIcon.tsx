d85c63fb151185f2631bae3af84769144a2dc81a
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-09-01 12:48:46 +0200

    Squashed 'vendor/db-scheduler-ui/' content from commit b8962dd
    git-subtree-dir: vendor/db-scheduler-ui
    git-subtree-split: b8962ddf32c319a8221ca8682a8a42b5f3a4225d

---
+++ db-scheduler-ui-frontend/src/assets/icons/PlayIcon.tsx
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) Bekk
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { createIcon } from '@chakra-ui/icons';
+import React from 'react';
+
+export const PlayIcon = createIcon({
+  displayName: 'Play',
+  viewBox: '0 0 512 512',
+  path: (
+    <path
+      stroke="currentColor"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      d="M133 440a35.37 35.37 0 01-17.5-4.67c-12-6.8-19.46-20-19.46-34.33V111c0-14.37 7.46-27.53 19.46-34.33a35.13 35.13 0 0135.77.45l247.85 148.36a36 36 0 010 61l-247.89 148.4A35.5 35.5 0 01133 440z"
+    />
+  ),
+});
